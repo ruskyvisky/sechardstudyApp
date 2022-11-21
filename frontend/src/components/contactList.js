bddfa4ce0c0c4c577676eaf69f0edf44a7bf2c88
Numan Dirican <numan_859@hotmail.com>
2022-11-21 21:59:50 +0300

    Delete Method Added

--- frontend/src/components/contactList.js
+++ frontend/src/components/contactList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Contact from "./contact"
+import axios from "axios"
 export default class ContactList extends Component {
  constructor(props){ 
     super(props)
@@ -10,7 +11,7 @@ export default class ContactList extends Component {
 
  }
  componentDidMount() {  
-        fetch("/contacts")
+        fetch("/contacts") // apiden fetch ediyorum.
         .then(res=> res.json().then(data => {
             this.setState({
                 contacts : data
@@ -19,13 +20,23 @@ export default class ContactList extends Component {
         
         )
 }
+
+delContact =  (contact) => { // DELETE METHOD APİ CALLS
+  console.log(contact)
+  axios.delete(`http://localhost:5000/contacts/${contact}`)
+  const newContact = this.state.contacts.filter(
+     (contactss) => contactss.id !== contact
+   );
+   this.setState({
+    contacts: newContact,
+   });
+}
     render() {
    const {contacts} = this.state; 
     return (
         <table className="table">
         <thead>
           <tr>
-          
             <th scope="col">Name</th>
             <th scope="col">Address </th>
             <th scope="col">Phone</th>
@@ -36,17 +47,30 @@ export default class ContactList extends Component {
         <tbody>
           
          {
+          
             contacts.map((contact,i) =>{
                 const {id,name,address,phone,mobile_phone,email} = contact;
-                return <Contact
-                key={i}
-                id = {id}
-                name = {name}
-                address = {address}
-                phone = {phone}
-                mobile_phone = {mobile_phone}
-                email = {email}
-                />
+                return (
+                  <tr key={i}>  
+                          <td>{name}</td>
+                          <td>{address}</td>
+                          <td>{phone}</td>
+                          <td>{mobile_phone}</td>
+                          <td>{email}</td>
+                          <td>
+                         {/* <DeleteButton /> */}
+                         <button className='btn btn-danger ' type='button' onClick={()=>{this.delContact(id)}} > Delete</button>
+                          </td>
+                          <td>
+                      
+                          </td>
+                        </tr>
+                        
+                          
+                          
+                      
+                    
+                  )
                 
             })
          }
